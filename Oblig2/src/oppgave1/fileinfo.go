b78a740a1b3100b75817a915b5bb35718708cf1d
Kristoffer Slettebakken <35600843+krissl15@users.noreply.github.com>
2018-02-26 14:43:32 +0100

    Update fileinfo.go

--- Oblig2/src/oppgave1/fileinfo.go
+++ Oblig2/src/oppgave1/fileinfo.go
@@ -4,6 +4,66 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Programmet skal returnere informasjon om en fil suhden")
+	mode()
+
+}
+
+func mode() {
+	fi, err := os.Lstat("hello.txt")
+	fileInfo, err := os.Lstat("hello.txt")
+	fileSize := float32(fileInfo.Size())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Information about", fileInfo.Name())
+	fmt.Println("Size : ", fileSize, "bytes,", fileSize/1000, "KB,", fileSize/1000000, "MB,", fileSize/1000000000, "GB")
+	switch mode := fi.Mode(); {
+	case mode.IsDir():
+		fmt.Println("directory")
+	case !mode.IsDir():
+		fmt.Println("not a directory")
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsRegular():
+		fmt.Println("regular file")
+	case !mode.IsRegular():
+		fmt.Println("not regular file")
+	}
+
+	fmt.Println("Has Unix permission bits : ", fileInfo.Mode().Perm())
+
+	switch mode := fi.Mode(); {
+	case mode&os.ModeAppend != 0:
+		fmt.Println("append only")
+	case mode&os.ModeAppend == 0:
+		fmt.Println("not append only")
+	}
+
+	switch mode := fi.Mode(); {
+	case mode&os.ModeDevice != 0:
+		fmt.Println("Is a device file")
+	case mode&os.ModeAppend == 0:
+		fmt.Println("Is not a device file")
+	}
+
+	switch mode := fi.Mode(); {
+	case mode&os.ModeCharDevice != 0:
+		fmt.Println("Is a unix character device")
+	case mode&os.ModeAppend == 0:
+		fmt.Println("Is not a unix character device")
+	}
+
+
+
+	switch mode := fi.Mode(); {
+	case mode&os.ModeSymlink != 0:
+		fmt.Println("symbolic link")
+	case mode&os.ModeSymlink == 0:
+		fmt.Println("not symbolic link")
+	}
+
 
 }
