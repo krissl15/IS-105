d33053ebf3b9d023a8f5f989525817134ee041c2
eileraas <35597561+eileraas@users.noreply.github.com>
2018-02-26 14:25:19 +0100

    Update filecount.go

--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -5,3 +5,32 @@ import "fmt"
 func main() {
 	fmt.Println("Programmet skal lese en tekst-fil, skrive ut totalt antall linjer og antall for fem runes som forekommer hyppigst i filen")
 }
+package Oblig2
+
+import (
+	"os"
+	"log"
+	"bufio"
+	"fmt"
+)
+
+var (
+	fileInfo os.FileInfo
+	err      error
+)
+
+func main() {
+	// Stat returns file info. It will return
+	// an error if there is no file.
+	fileInfo, err = os.Stat("Textcounter.rtf")
+	if err != nil {
+		log.Fatal(err)
+
+
+file, _ := os.Open("/path/to/desktop/Textcounter")
+fileScanner := bufio.NewScanner(file)
+lineCount := 0
+for fileScanner.Scan() {
+lineCount++
+}
+fmt.Println("number of lines:", lineCount)
