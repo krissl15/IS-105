505c6a3d081982d96a2c4640687f78072b171856
Kristoffer Slettebakken <35600843+krissl15@users.noreply.github.com>
2018-03-01 05:33:33 -0800

    Update fileinfo.go

--- Oblig2/src/oppgave1/fileinfo.go
+++ Oblig2/src/oppgave1/fileinfo.go
@@ -1,69 +1,94 @@
 package main
 
-import "fmt"
+import ("fmt"
+	"os"
+	"log"
+)
+
 
 func main() {
-
-	mode()
+	filename := os.Args[1] // Må nå ha et argument for å kjøre i cmd
+	fileInfo(filename)
 
 }
 
-func mode() {
-	fi, err := os.Lstat("hello.txt")
-	fileInfo, err := os.Lstat("hello.txt")
-	fileSize := float32(fileInfo.Size())
+func fileInfo(s string) {
+	file, err := os.Lstat(s)
+
+
+
+	bytes := float64(file.Size())
+	KB := bytes / 1024
+	MB := KB / 1024
+	GB := MB / 1024
+
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Information about", fileInfo.Name())
-	fmt.Println("Size : ", fileSize, "bytes,", fileSize/1000, "KB,", fileSize/1000000, "MB,", fileSize/1000000000, "GB")
-	switch mode := fi.Mode(); {
-	case mode.IsDir():
-		fmt.Println("directory")
-	case !mode.IsDir():
-		fmt.Println("not a directory")
+	fmt.Println("Information about", file.Name())
+	fmt.Printf("%v, %f, %v, %f, %v %f %v %.15g %v\n ", "Size :", bytes, "bytes,", KB, "KB,", MB, "MB,", GB, "GB")
+
+	if file.Mode().IsDir() == true {
+		fmt.Println("Is a directory")
+	} else if file.Mode().IsDir() == false {
+		fmt.Println("Is not a directory")
 	}
 
-	switch mode := fi.Mode(); {
-	case mode.IsRegular():
-		fmt.Println("regular file")
-	case !mode.IsRegular():
-		fmt.Println("not regular file")
+	if file.Mode().IsRegular() { //
+		fmt.Println("Is a regular file") // hvis det er en file, bli det true
+	} else {
+		fmt.Println("Is not a regular file") // hvis det er bare en mappe bli det false
 	}
 
-	fmt.Println("Has Unix permission bits : ", fileInfo.Mode().Perm())
+	fmt.Println("Has Unix permission bits:", file.Mode().Perm()) // Hvilke retigheter man har på filen
 
-	switch mode := fi.Mode(); {
-	case mode&os.ModeAppend != 0:
-		fmt.Println("append only")
-	case mode&os.ModeAppend == 0:
-		fmt.Println("not append only")
+	if file.Mode()&os.ModeAppend == os.ModeAppend {          // Deler filen data/info med andre filer
+		fmt.Println("Is append only")
+	} else {
+		fmt.Println("Is not append only")
 	}
 
-	switch mode := fi.Mode(); {
-	case mode&os.ModeDevice != 0:
+	if file.Mode()&os.ModeDevice == os.ModeDevice { // En fil som lar software kommunisere med "device driver" ved hjelp av standar input/output system calls
 		fmt.Println("Is a device file")
-	case mode&os.ModeAppend == 0:
-		fmt.Println("Is not a device file")
+	} else {
+		fmt.Println("Is a device file")
 	}
 
-	switch mode := fi.Mode(); {
-	case mode&os.ModeCharDevice != 0:
-		fmt.Println("Is a unix character device")
-	case mode&os.ModeAppend == 0:
-		fmt.Println("Is not a unix character device")
+	if file.Mode()&os.ModeCharDevice == os.ModeCharDevice {
+		fmt.Println("Is a Unix character device")
+	} else {
+		fmt.Println("Is not Unix character device")
 	}
 
-
-
-	switch mode := fi.Mode(); {
-	case mode&os.ModeSymlink != 0:
-		fmt.Println("symbolic link")
-	case mode&os.ModeSymlink == 0:
-		fmt.Println("not symbolic link")
+	if file.Mode()&os.ModeSocket == os.ModeSocket{
+		fmt.Println("Is a unix block device")
+	}else {
+		fmt.Println("Is not a unix block device")
 	}
 
+	if file.Mode()&os.ModeSymlink == os.ModeSymlink { // Det er en symlink
+		fmt.Println("Is a symbolic link")
+	} else {
+		fmt.Println("Is not a symbolic link")
+	}
 
 }
+/**
+Is/Is not a directory
+
+Is/Is not a regular file
+
+Has Unix permission bits: -rwxrwxrwx
+
+Is/Is not append only
+
+Is/Is not a device file
+
+Is/Is not a Unix character device
+
+Is/Is not a Unix block device
+
+Is/Is not a symbolic link
+*/
