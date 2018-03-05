72a25f79685a68ee700689868f35c28cc598b096
Kristoffer Slettebakken <35600843+krissl15@users.noreply.github.com>
2018-03-04 16:00:33 -0800

    Add files via upload

--- Oblig2/src/oppgave3/addtofile.go
+++ Oblig2/src/oppgave3/addtofile.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"log"
+	"io/ioutil"
+	"fmt"
+)
+
+
 
 func main() {
-	fmt.Println("Oppgave 3")
+
+
+    file, err :=  ioutil.ReadFile("numbers.txt") //åpner filen for lesing, variabelen "file" blir []byte
+    fileText := string(file) //gjør om file (som er []byte) til string
+	check(err)//quickie
+
+    if len(os.Args) == 3 { //sjekker om det er 3 argument når programmet kjøres (filnavn, tall 1, tall2)
+
+		nrOne := os.Args[1] //os.args blir en slice, plass [1] blir første tallet som skrives (etter filnavn)
+		nrTwo := os.Args[2]// [2] blir tall 2
+		createFile(nrOne, nrTwo) // kjør createfile med tallene skrevet i cmd som parametre
+	}else{ //om det ikke er skrevet noen tall når programmet kjøres, vil det som står i tekstfilen printes til kommando
+		fmt.Printf("%q",fileText)
+
+	}
+
 }
+
+func createFile(one, two string) {
+	f, err := os.Create("numbers.txt") //lag en fil som heter "numbers.txt"
+	check(err)
+	defer f.Close()
+	nums := []byte(one + " " + two) //lag en []byte med parametrene
+	f.Write(nums) //skriver parametrene inn i "numbers.txt"
+
+}
+
+func check(err error){ //så jeg slipper den gad damn if-setninga overalt
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+
+}
+
+
--- Oblig2/src/oppgave3/sumfromfile.go
+++ Oblig2/src/oppgave3/sumfromfile.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
 
-func main() {
-	fmt.Println("Oppgave 3")
+	"io/ioutil"
+	"log"
+	"strconv"
+	"os"
+	"strings"
+
+
+
+)
+
+func main()  {
+	content, err := ioutil.ReadFile("numbers.txt") // åpner for å bruke ioutil
+	file, err := os.OpenFile("numbers.txt", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666, )//åpner for å bruke os.Write.
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	strSlice := string(content) //gjør teksten i filen "numbers" til string
+	numbers := strings.Split(strSlice, " ") //tallene i "numbers.txt" har mellomrom mellom seg.
+	nrOne := numbers[0]							//hadde jeg ikke splittet, men brukt []plassene, ville dette skapt
+	nrTwo := numbers[1]							//problem om tallene har flere siffer
+
+	intOne, err := strconv.Atoi(nrOne) //convert tallene til ints
+	intTwo, err := strconv.Atoi(nrTwo)
+
+   sum := intOne+intTwo //summer tallene
+	s := strconv.Itoa(sum) //gjør summen til string
+	b := []byte(s)//gjør stringen til []byte
+
+	file.Write(b) //skriv inn []byten til filen
+
+
+
+
+
+
+
 }
